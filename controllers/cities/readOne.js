94177794c1b9a7f48cd7cbbf34a1337083c3e7ef
atutito <augustoalerino@hotmail.com>
2023-08-15 23:07:49 -0300

    queries mejoradas

--- controllers/cities/readOne.js
+++ controllers/cities/readOne.js
@@ -2,7 +2,7 @@ import City from "../../models/City.js"
 
 export default async(req,res,next)=>{
     try{
-      let readOneCity = await City.findOne({_id:req.params.city_id}).select("country photo -_id")
+      let readOneCity = await City.findById({_id:req.params.city_id}).select("city country photo -_id")
       if(readOneCity){
       return res.status(200).json({
         success: true,
--- middlewares/notFoundHandler.js
+++ middlewares/notFoundHandler.js
@@ -1,6 +1,6 @@
 export default function(req, res, next) {
     return res.status(404).json({
       success: false,
-      message : "Route not found " +req.url+ ' @ '+req.method,
+      message : "Route not found " + req.url + ' @ '+ req.method,
     })
   }
