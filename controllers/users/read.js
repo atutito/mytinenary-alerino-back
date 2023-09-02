93d1acb53554c05bef14f20c7d7aa41a1311f174
atutito <augustoalerino@hotmail.com>
2023-09-02 16:38:50 -0300

    final

--- controllers/users/read.js
+++ controllers/users/read.js
@@ -2,7 +2,7 @@ import User from "../../models/User.js"
 
 export default async(req,res,next)=>{
     try{
-      let readUser = await User.find()
+      let readUser = await User.find().sort({name:1}).limit(6)
       return res.status(200).json({
         success: true,
         message: "users read",
@@ -10,6 +10,9 @@ export default async(req,res,next)=>{
       })
     }
     catch(err){
-      next(err)
-    }
-  }
+      return res.status(400).json({
+        success: false,
+        message: 'not found',
+        response: null
+    })
+  }}
