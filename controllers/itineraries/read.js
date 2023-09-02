3dc4eeca9828d160e505847ec4319f9f86fc3d5a
atutito <augustoalerino@hotmail.com>
2023-09-02 17:52:04 -0300

    final

--- controllers/itineraries/read.js
+++ controllers/itineraries/read.js
@@ -6,7 +6,7 @@ export default async (req,res,next) => {
         if (req.query.city_id) {queries.city_id = req.query.city_id}
         let readItineraries = await Itinerary
             .find(queries,'-__v -createdAt -updatedAt')
-            .populate({path:'city_id', select:"city photo admin_id", populate:{ path: 'admin_id', select:"name"}} )
+            .populate({path:'city_id', select:"city photo admin_id", populate:{ path: 'admin_id', select:"name photo"}} )
             return res.status(200).json({
                 success: true,
                 message: 'itineraries found',
